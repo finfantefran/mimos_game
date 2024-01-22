8480eaa1569c3f23bd2ef745d9a4ff9ef35e3b42
finfantefran <63743054+finfantefran@users.noreply.github.com>
2024-01-23 00:30:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-document.getElementById('no-button').addEventListener('mouseover', function () {
+
+function moveButton() {
     const button = document.getElementById('no-button');
-    const maxX = 200;
-    const maxY = 200;
+    const maxX = 100;
+    const maxY = 100;
     const randomX = Math.random() * maxX;
     const randomY = Math.random() * maxY;
     button.style.transform = `translate(${randomX}px, ${randomY}px)`;
-    noButton.addEventListener('mouseover', moveButton);
-    noButton.addEventListener('touchstart', moveButton);
-});
+}
+
+
+const noButton = document.getElementById('no-button');
+noButton.addEventListener('mouseover', moveButton); 
+noButton.addEventListener('touchstart', moveButton); 
+
 
 document.getElementById('yes-button').addEventListener('click', function() {
-    document.querySelector('h1').textContent = 'No eres un toxo';
+    document.querySelector('h1').textContent = 'Eres un toxo';
 });
