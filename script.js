b498d74537b8b065f9357925742a8f98f36be3f9
finfantefran <63743054+finfantefran@users.noreply.github.com>
2024-01-23 00:26:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.getElementById('no-button').addEventListener('mouseover', function () {
     const button = document.getElementById('no-button');
-    const maxX = 100;
-    const maxY = 100;
+    const maxX = 200;
+    const maxY = 200;
     const randomX = Math.random() * maxX;
     const randomY = Math.random() * maxY;
     button.style.transform = `translate(${randomX}px, ${randomY}px)`;
+    noButton.addEventListener('touchstart', moveButton);
 });
